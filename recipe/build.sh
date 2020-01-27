32b88983e3b98035b42a895be35d46a7cfaad1d5
Giovanni <giovanni.bussi@gmail.com>
2020-01-27 11:50:00 +0100

    Update build.sh
    Enabled internal asmjit

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,9 @@ if [[ $(uname) == "Linux" ]]; then
 # When using env modules the dependent libraries can be found through the
 # LD_LIBRARY_PATH or encoded configuring with -rpath.
 # Conda does not use LD_LIBRARY_PATH and it is thus necessary to suggest where libraries are.
-  export STATIC_LIBS=-Wl,-rpath-link,$PREFIX/lib
+  export STATIC_LIBS=-Wl,-rpath-link,$PREFIX/lib  
+# -lrt is required to link clock_gettime
+  export LIBS="-lrt $LIBS"
 fi
 
 # we also store path so that software linking libplumedWrapper.a knows where libplumedKernel can be found.
@@ -21,7 +23,8 @@ export LIBS="-lgsl -lgslcblas -llapack -lblas -lxdrfile -lz $LIBS"
 # --disable-libsearch forces to link only explicitely requested libraries
 # --disable-static-patch avoid tests that are only required for static patches
 # --disable-static-archive makes package smaller
-./configure --prefix=$PREFIX --disable-python --disable-libsearch --disable-static-patch --disable-static-archive
+# --enable-asmjit enables bundled asmjit implementation
+./configure --prefix=$PREFIX --disable-python --disable-libsearch --disable-static-patch --disable-static-archive --enable-asmjit
 
 make -j${CPU_COUNT}
 make install
